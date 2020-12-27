fca84358876a6626154d6f5a96a2a0f4303f180e
cn005897 <1098120843@qq.com>
2020-12-27 22:28:52 +0800

    reflash

--- layui/index.js
+++ layui/index.js
@@ -3,11 +3,8 @@ window.onload = function () {
     if (sessionStorage.getItem("user") != null) {
         window.parent.document.getElementById("uid").innerHTML = sessionStorage.getItem("user");
         window.parent.document.getElementById("loginTo").innerHTML = "注销";
-       // $(location).attr('href', 'default.html');
     }
-    var state = window.parent.document.getElementById("loginTo").innerHTML;
-    //if (state == "注销") {
-    //    parent.window.location.reload();
-    //    sessionStorage.clear();
-    //}
+    if (sessionStorage.getItem("user") == null) {
+        $(location).attr('href', 'login.html');
+    }
 }
